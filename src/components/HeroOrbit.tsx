6f6bac3a4471d61124981dd9b483f9e1a49c5081
Shresth <shresthdeorari38@gmail.com>
2024-12-12 13:31:46 +0530

    Intial Commit for Portfolio Website

---
+++ src/components/HeroOrbit.tsx
@@ -0,0 +1,17 @@
+import {PropsWithChildren} from 'react'
+
+
+const HeroOrbit = ({children ,size, rotation} : PropsWithChildren<{size:Number, rotation:Number}>) => {
+  return (
+    <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <div className='flex items-start justify-start'
+        style={{height:`${size}px`, width:`${size}px`, transform:`rotate(${rotation}deg)`}}>
+          <div className='inline-flex ' style={{transform:`rotate(${-rotation}deg)`}}>
+            {children}
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default HeroOrbit
