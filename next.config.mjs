1f8325eb00e539f5a9b49f95daa24cea010e3ff5
Shresth <shresthdeorari38@gmail.com>
2024-12-13 17:02:30 +0530

    final changes hopefully

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'build', // Specify your custom build directory here
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
+    // Same custom SVG handling as before
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg")
     );
 
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
         resourceQuery: /url/, // *.svg?url
       },
-      // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
@@ -38,7 +37,6 @@ const nextConfig = {
       }
     );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
